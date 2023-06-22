198f58d3b4a8a99709ba1915c453ceafd07947ba
Mike Verderese <mike@useodin.com>
2023-06-22 12:20:24 -0400

    Update Readme and install scripts

--- remove.sh
+++ remove.sh
@@ -5,7 +5,6 @@ systemctl stop wg-quick@wg0
 systemctl disable wg-quick@wg0
 
 yes | apt autoremove wireguard wireguard-dkms wireguard-tools
-#yes | apt autoremove software-properties-common
 yes | apt update
 
 rm -rf /etc/wireguard
