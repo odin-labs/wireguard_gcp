44767f702c1375a15f1d263b1aa71df97045bd87
Mike Verderese <mike@useodin.com>
2023-06-22 13:07:52 -0400

    Update wireguard install script

--- remove.sh
+++ remove.sh
@@ -12,8 +12,8 @@ wg-quick down wg0
 systemctl stop wg-quick@wg0
 systemctl disable wg-quick@wg0
 
-yes | apt autoremove wireguard wireguard-dkms wireguard-tools
-yes | apt update
+apt autoremove -y wireguard wireguard-dkms wireguard-tools
+apt update -y
 
 rm -rf /etc/wireguard
 
